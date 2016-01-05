be8b960403745bd83cef9230869a79fd74b7b270
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-01-05 17:23:05 -0200

    javascript sandbox added

---
+++ Javascript-SandBox/string.js
@@ -0,0 +1,18 @@
+var x = 'Marcell';
+console.log('charAt: '+x.charAt(0));
+console.log('charCodeAt:'+x.charCodeAt(1));
+console.log('concat: '+x.concat(' monteiro'));
+console.log('indexOf: '+x.indexOf('r'));
+console.log('lastIndexOf: '+x.lastIndexOf('l'));
+console.log('match: '+x.match('l'));
+console.log('replace: '+x.replace('l', 'U'));
+console.log('search: '+x.search('m'));
+console.log('slice: '+x.slice(1, 3));
+console.log('split: '+x.split('c'));
+console.log('split: '+x.split);
+console.log('substr: '+x.substr(1, 4));
+console.log('substring: '+x.substring(1, 4));
+console.log('toLowerCase: '+x.toLowerCase());
+console.log('toString: '+x.toString());
+console.log('trim: '+x.trim())
+
