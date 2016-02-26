babd328568ce8c3f51044164a18c9726bbc77041
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-02-26 19:37:39 -0300

    starting to create resource ember rails

--- thor-sandbox/src/cli.rb
+++ thor-sandbox/src/cli.rb
@@ -4,17 +4,33 @@ module MyGem
 	VERSION = '0.1.0'
 
 	class Cli < Thor
+		include Thor::Actions
+		check_unknown_options!
+
+		def self.exit_on_failure?
+	    true
+		end
+
 		desc 'version', 'Display version'
 		map %w[-v --version] => :version
+
+		class_option 'verbose',  :type => :boolean, :default => false
 
 		def version
 			say "MyGem #{VERSION}"
 		end
 
 		desc 'new PATH', 'Create a new static website'
+		option :javascript_engine, :default => 'babeljs', :aliases => '-j'
+		option :file, :type => :array, :aliases => :files
+		option :force, :type => :boolean, :default => false
+		#option :database, :required => true
 		def new(path)
 			path = File.expand_path(path)
+			raise Error, set_color("ERROR: #{path} already exists.", :red) if File.exist?(path)
+
 			say "Creating site at #{path}"
+			say options
 		end
 	end
 end
