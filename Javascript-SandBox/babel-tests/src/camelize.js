1a427301c7339d42fd8ddee8d0013d0e58004f66
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-07-17 15:59:25 -0300

    camelize included in babel-test for ember-cli-mirage

---
+++ Javascript-SandBox/babel-tests/src/camelize.js
@@ -0,0 +1,18 @@
+const STRING_CAMELIZE_REGEXP_1 = (/(\-|\_|\.|\s)+(.)?/g);
+const STRING_CAMELIZE_REGEXP_2 = (/(^|\/)([A-Z])/g);
+
+const CAMELIZE_CACHE = new Cache(1000, function(key) {
+	return key.replace(STRING_CAMELIZE_REGEXP_1, function(match, separator, chr) {
+		return chr ? chr.toUpperCase() : '';
+	}).replace(STRING_CAMELIZE_REGEXP_2, function(match, separator, chr) {
+		return match.toLowerCase();
+	});
+});
+
+function camelize(str) {
+	return CAMELIZE_CACHE.get(str);
+}
+
+export { camelize };
+
+
