61bd6340101a72bc2b0c749c708646fef07e053d
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-07-20 09:28:00 -0300

    testing ember addons

---
+++ Javascript-SandBox/ember-sandbox/dist/assets/ember-sandbox.js
@@ -0,0 +1,416 @@
+"use strict";
+
+/* jshint ignore:start */
+
+
+
+/* jshint ignore:end */
+
+define('ember-sandbox/app', ['exports', 'ember', 'ember-sandbox/resolver', 'ember-load-initializers', 'ember-sandbox/config/environment'], function (exports, _ember, _emberSandboxResolver, _emberLoadInitializers, _emberSandboxConfigEnvironment) {
+
+  var App = undefined;
+
+  _ember['default'].MODEL_FACTORY_INJECTIONS = true;
+
+  App = _ember['default'].Application.extend({
+    modulePrefix: _emberSandboxConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _emberSandboxConfigEnvironment['default'].podModulePrefix,
+    Resolver: _emberSandboxResolver['default']
+  });
+
+  (0, _emberLoadInitializers['default'])(App, _emberSandboxConfigEnvironment['default'].modulePrefix);
+
+  exports['default'] = App;
+});
+define('ember-sandbox/components/app-version', ['exports', 'ember-cli-app-version/components/app-version', 'ember-sandbox/config/environment'], function (exports, _emberCliAppVersionComponentsAppVersion, _emberSandboxConfigEnvironment) {
+
+  var name = _emberSandboxConfigEnvironment['default'].APP.name;
+  var version = _emberSandboxConfigEnvironment['default'].APP.version;
+
+  exports['default'] = _emberCliAppVersionComponentsAppVersion['default'].extend({
+    version: version,
+    name: name
+  });
+});
+define('ember-sandbox/helpers/pluralize', ['exports', 'ember-inflector/lib/helpers/pluralize'], function (exports, _emberInflectorLibHelpersPluralize) {
+  exports['default'] = _emberInflectorLibHelpersPluralize['default'];
+});
+define('ember-sandbox/helpers/singularize', ['exports', 'ember-inflector/lib/helpers/singularize'], function (exports, _emberInflectorLibHelpersSingularize) {
+  exports['default'] = _emberInflectorLibHelpersSingularize['default'];
+});
+define('ember-sandbox/initializers/app-version', ['exports', 'ember-cli-app-version/initializer-factory', 'ember-sandbox/config/environment'], function (exports, _emberCliAppVersionInitializerFactory, _emberSandboxConfigEnvironment) {
+  exports['default'] = {
+    name: 'App Version',
+    initialize: (0, _emberCliAppVersionInitializerFactory['default'])(_emberSandboxConfigEnvironment['default'].APP.name, _emberSandboxConfigEnvironment['default'].APP.version)
+  };
+});
+define('ember-sandbox/initializers/container-debug-adapter', ['exports', 'ember-resolver/container-debug-adapter'], function (exports, _emberResolverContainerDebugAdapter) {
+  exports['default'] = {
+    name: 'container-debug-adapter',
+
+    initialize: function initialize() {
+      var app = arguments[1] || arguments[0];
+
+      app.register('container-debug-adapter:main', _emberResolverContainerDebugAdapter['default']);
+      app.inject('container-debug-adapter:main', 'namespace', 'application:main');
+    }
+  };
+});
+define('ember-sandbox/initializers/data-adapter', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `data-adapter` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'data-adapter',
+    before: 'store',
+    initialize: _ember['default'].K
+  };
+});
+define('ember-sandbox/initializers/ember-cli-mirage', ['exports', 'ember-cli-mirage/utils/read-modules', 'ember-sandbox/config/environment', 'ember-sandbox/mirage/config', 'ember-cli-mirage/server', 'lodash/object/assign'], function (exports, _emberCliMirageUtilsReadModules, _emberSandboxConfigEnvironment, _emberSandboxMirageConfig, _emberCliMirageServer, _lodashObjectAssign) {
+  exports.startMirage = startMirage;
+  exports['default'] = {
+    name: 'ember-cli-mirage',
+    initialize: function initialize(application) {
+      if (arguments.length > 1) {
+        // Ember < 2.1
+        var container = arguments[0],
+            application = arguments[1];
+      }
+
+      if (_shouldUseMirage(_emberSandboxConfigEnvironment['default'].environment, _emberSandboxConfigEnvironment['default']['ember-cli-mirage'])) {
+        startMirage(_emberSandboxConfigEnvironment['default']);
+      }
+    }
+  };
+
+  function startMirage() {
+    var env = arguments.length <= 0 || arguments[0] === undefined ? _emberSandboxConfigEnvironment['default'] : arguments[0];
+
+    var environment = env.environment;
+    var modules = (0, _emberCliMirageUtilsReadModules['default'])(env.modulePrefix);
+    var options = (0, _lodashObjectAssign['default'])(modules, { environment: environment, baseConfig: _emberSandboxMirageConfig['default'], testConfig: _emberSandboxMirageConfig.testConfig });
+
+    return new _emberCliMirageServer['default'](options);
+  }
+
+  function _shouldUseMirage(env, addonConfig) {
+    var userDeclaredEnabled = typeof addonConfig.enabled !== 'undefined';
+    var defaultEnabled = _defaultEnabled(env, addonConfig);
+
+    return userDeclaredEnabled ? addonConfig.enabled : defaultEnabled;
+  }
+
+  /*
+    Returns a boolean specifying the default behavior for whether
+    to initialize Mirage.
+  */
+  function _defaultEnabled(env, addonConfig) {
+    var usingInDev = env === 'development' && !addonConfig.usingProxy;
+    var usingInTest = env === 'test';
+
+    return usingInDev || usingInTest;
+  }
+});
+define('ember-sandbox/initializers/ember-data', ['exports', 'ember-data/setup-container', 'ember-data/-private/core'], function (exports, _emberDataSetupContainer, _emberDataPrivateCore) {
+
+  /*
+  
+    This code initializes Ember-Data onto an Ember application.
+  
+    If an Ember.js developer defines a subclass of DS.Store on their application,
+    as `App.StoreService` (or via a module system that resolves to `service:store`)
+    this code will automatically instantiate it and make it available on the
+    router.
+  
+    Additionally, after an application's controllers have been injected, they will
+    each have the store made available to them.
+  
+    For example, imagine an Ember.js application with the following classes:
+  
+    App.StoreService = DS.Store.extend({
+      adapter: 'custom'
+    });
+  
+    App.PostsController = Ember.ArrayController.extend({
+      // ...
+    });
+  
+    When the application is initialized, `App.ApplicationStore` will automatically be
+    instantiated, and the instance of `App.PostsController` will have its `store`
+    property set to that instance.
+  
+    Note that this code will only be run if the `ember-application` package is
+    loaded. If Ember Data is being used in an environment other than a
+    typical application (e.g., node.js where only `ember-runtime` is available),
+    this code will be ignored.
+  */
+
+  exports['default'] = {
+    name: 'ember-data',
+    initialize: _emberDataSetupContainer['default']
+  };
+});
+define('ember-sandbox/initializers/export-application-global', ['exports', 'ember', 'ember-sandbox/config/environment'], function (exports, _ember, _emberSandboxConfigEnvironment) {
+  exports.initialize = initialize;
+
+  function initialize() {
+    var application = arguments[1] || arguments[0];
+    if (_emberSandboxConfigEnvironment['default'].exportApplicationGlobal !== false) {
+      var value = _emberSandboxConfigEnvironment['default'].exportApplicationGlobal;
+      var globalName;
+
+      if (typeof value === 'string') {
+        globalName = value;
+      } else {
+        globalName = _ember['default'].String.classify(_emberSandboxConfigEnvironment['default'].modulePrefix);
+      }
+
+      if (!window[globalName]) {
+        window[globalName] = application;
+
+        application.reopen({
+          willDestroy: function willDestroy() {
+            this._super.apply(this, arguments);
+            delete window[globalName];
+          }
+        });
+      }
+    }
+  }
+
+  exports['default'] = {
+    name: 'export-application-global',
+
+    initialize: initialize
+  };
+});
+define('ember-sandbox/initializers/injectStore', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `injectStore` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'injectStore',
+    before: 'store',
+    initialize: _ember['default'].K
+  };
+});
+define('ember-sandbox/initializers/store', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `store` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'store',
+    after: 'ember-data',
+    initialize: _ember['default'].K
+  };
+});
+define('ember-sandbox/initializers/transforms', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `transforms` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'transforms',
+    before: 'store',
+    initialize: _ember['default'].K
+  };
+});
+define("ember-sandbox/instance-initializers/ember-data", ["exports", "ember-data/-private/instance-initializers/initialize-store-service"], function (exports, _emberDataPrivateInstanceInitializersInitializeStoreService) {
+  exports["default"] = {
+    name: "ember-data",
+    initialize: _emberDataPrivateInstanceInitializersInitializeStoreService["default"]
+  };
+});
+define("ember-sandbox/mirage/config", ["exports"], function (exports) {
+  exports["default"] = function () {
+
+    // These comments are here to help you get started. Feel free to delete them.
+
+    /*
+      Config (with defaults).
+       Note: these only affect routes defined *after* them!
+    */
+
+    // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+    // this.namespace = '';    // make this `api`, for example, if your API is namespaced
+    // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+
+    /*
+      Shorthand cheatsheet:
+       this.get('/posts');
+      this.post('/posts');
+      this.get('/posts/:id');
+      this.put('/posts/:id'); // or this.patch
+      this.del('/posts/:id');
+       http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
+    */
+  };
+});
+define("ember-sandbox/mirage/scenarios/default", ["exports"], function (exports) {
+  exports["default"] = function () /* server */{
+
+    /*
+      Seed your development database using your factories.
+      This data will not be loaded in your tests.
+       Make sure to define a factory for each model you want to create.
+    */
+
+    // server.createList('post', 10);
+  };
+});
+define('ember-sandbox/mirage/serializers/application', ['exports', 'ember-cli-mirage'], function (exports, _emberCliMirage) {
+  exports['default'] = _emberCliMirage.JSONAPISerializer.extend({});
+});
+define('ember-sandbox/resolver', ['exports', 'ember-resolver'], function (exports, _emberResolver) {
+  exports['default'] = _emberResolver['default'];
+});
+define('ember-sandbox/router', ['exports', 'ember', 'ember-sandbox/config/environment'], function (exports, _ember, _emberSandboxConfigEnvironment) {
+
+  var Router = _ember['default'].Router.extend({
+    location: _emberSandboxConfigEnvironment['default'].locationType
+  });
+
+  Router.map(function () {});
+
+  exports['default'] = Router;
+});
+define('ember-sandbox/services/ajax', ['exports', 'ember-ajax/services/ajax'], function (exports, _emberAjaxServicesAjax) {
+  Object.defineProperty(exports, 'default', {
+    enumerable: true,
+    get: function get() {
+      return _emberAjaxServicesAjax['default'];
+    }
+  });
+});
+define('ember-sandbox/snippets/test', ['exports'], function (exports) {
+  console.log('test!!!!!!!!!');
+});
+define("ember-sandbox/templates/application", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template((function () {
+    return {
+      meta: {
+        "fragmentReason": {
+          "name": "missing-wrapper",
+          "problems": ["multiple-nodes", "wrong-type"]
+        },
+        "revision": "Ember@2.5.1",
+        "loc": {
+          "source": null,
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 4,
+            "column": 0
+          }
+        },
+        "moduleName": "ember-sandbox/templates/application.hbs"
+      },
+      isEmpty: false,
+      arity: 0,
+      cachedFragment: null,
+      hasRendered: false,
+      buildFragment: function buildFragment(dom) {
+        var el0 = dom.createDocumentFragment();
+        var el1 = dom.createElement("h2");
+        dom.setAttribute(el1, "id", "title");
+        var el2 = dom.createTextNode("Welcome to Ember");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n\n");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createComment("");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n");
+        dom.appendChild(el0, el1);
+        return el0;
+      },
+      buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
+        var morphs = new Array(1);
+        morphs[0] = dom.createMorphAt(fragment, 2, 2, contextualElement);
+        return morphs;
+      },
+      statements: [["content", "outlet", ["loc", [null, [3, 0], [3, 10]]]]],
+      locals: [],
+      templates: []
+    };
+  })());
+});
+define('ember-sandbox/tests/mirage/mirage/config.jshint', ['exports'], function (exports) {
+  QUnit.module('JSHint | mirage/config.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'mirage/config.js should pass jshint.');
+  });
+});
+define('ember-sandbox/tests/mirage/mirage/scenarios/default.jshint', ['exports'], function (exports) {
+  QUnit.module('JSHint | mirage/scenarios/default.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'mirage/scenarios/default.js should pass jshint.');
+  });
+});
+define('ember-sandbox/tests/mirage/mirage/serializers/application.jshint', ['exports'], function (exports) {
+  QUnit.module('JSHint | mirage/serializers/application.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'mirage/serializers/application.js should pass jshint.');
+  });
+});
+/* jshint ignore:start */
+
+
+
+/* jshint ignore:end */
+
+/* jshint ignore:start */
+
+define('ember-sandbox/config/environment', ['ember'], function(Ember) {
+  var prefix = 'ember-sandbox';
+/* jshint ignore:start */
+
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = Ember['default'].$('meta[name="' + metaName + '"]').attr('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  return { 'default': config };
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+/* jshint ignore:end */
+
+});
+
+/* jshint ignore:end */
+
+/* jshint ignore:start */
+
+if (!runningTests) {
+  require("ember-sandbox/app")["default"].create({"name":"ember-sandbox","version":"0.0.0+ff3b7b1f"});
+}
+
+/* jshint ignore:end */
+//# sourceMappingURL=ember-sandbox.map
