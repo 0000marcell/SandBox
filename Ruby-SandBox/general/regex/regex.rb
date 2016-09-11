764e2961d0d79d0fc96677bdc0a58b30bb4b8a30
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-09-11 19:23:47 -0300

    more examples added to ruby regex

--- Ruby-SandBox/general/regex/regex.rb
+++ Ruby-SandBox/general/regex/regex.rb
@@ -1,13 +1,44 @@
-# only letters 
-
 name = 'marcell'
+
+# only letters 
+puts "only letters"
 puts name =~ /marcell/ # 0
+puts "\n"
 
 # dot 
+puts "dot match"
 puts name =~ /./ # 0
+puts "\n"
 
 # one character at a time 
-
+puts "one character at a time"
 puts name =~ /[a]/ # 1
+puts "\n"
 
- 
+# set 
+puts "set of characters"
+puts name =~ /[acl]/ # 1
+puts "\n"
+
+# range
+puts "range of characters"
+puts "bcd" =~ /[b-c]/
+puts "bcd" =~ /b-d/ # not gonna match 
+puts "\n"
+
+# comman sets
+puts "common sets"
+puts "badfgs" =~ /\w/ # any letter
+puts "   " =~ /\s/ # match white space tabs
+puts "1232424adbsdf" =~ /\d/ # match any digit
+puts "\n"
+
+# separate statements
+puts "separate statements with ()"
+puts "bcdzzzzzz123" =~ /bcd.*123|234/ # 0
+puts "\n"
+
+# or character
+puts "or character |"
+puts "bcd" =~ /(bcd)|123/ # 0
+puts "\n"
