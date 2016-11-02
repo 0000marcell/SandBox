555c21417bf047e41a19693f10edd4b7162d6a30
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-11-02 18:47:12 -0200

    testing out origamijs

---
+++ javascript/origami/js/index.js
@@ -0,0 +1,10 @@
+var style = {
+	color: '#000',
+	font: '70px Helvetica',
+	align: 'center',
+	border: '2px solid gold'
+};
+
+origami('#canvas-id')
+  .text("Nice!", 100, 100, style)
+	.text("Really Nice!", 150, 150, style).draw();
