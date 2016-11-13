adfea446b6ca5bbda7dbcd30459df5664d37fe4d
Marcell Monteiro Cruz <0000marcell@gmail.com>
2016-11-12 22:37:48 -0200

    showing flash messages in materialize v1

--- javascript/ember/ember-v1-materialize/app/controllers/application.js
+++ javascript/ember/ember-v1-materialize/app/controllers/application.js
@@ -4,16 +4,12 @@ export default Ember.Controller.extend({
 	flashMessages: Ember.inject.service(),
 	actions: {
 		stickToast(){
-			console.log('click!');
-			this.get('flashMessages').success('Thanks for signing up!');
-			/*
 			this.get('flashMessages').add({
 				icon: 'flight_land',
 				color: 'yellow purple-text rounded',
 				sticky: true,
 				message: 'This message won\'t disappear until clicked'
 			});	
-			*/
 		}
 	}
 });
