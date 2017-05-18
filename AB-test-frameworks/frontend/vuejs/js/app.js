e981ea346c523c5c89d18a740d24dbd6d84cf437
Marcell Monteiro Cruz <0000marcell@gmail.com>
2017-05-18 16:44:56 -0300

    testing vue cli

--- AB-test-frameworks/frontend/vuejs/js/app.js
+++ AB-test-frameworks/frontend/vuejs/js/app.js
@@ -1,103 +1,90 @@
-var data = [
-  { id: 1, name: 'buy new socks',    active: true},
-  { id: 2, name: 'buy other things', active: true},
-  { id: 3, name: 'buy a clock', active: false },
-  { id: 4, name: 'buy a car', active: false }
-];
+var data = [{id: 1, name: 'buy new socks',
+  active: true},
+  {id: 2, name: 'buy new things', active: true},
+  {id: 3, name: 'buy another things', active: false}];
 
-Vue.component('todo-item', {
-  props: ['todo', 'deactivateTodo'],
+var todoList = Vue.component('todo-list', {
+  props: ['createTodo', 'deactivateTodo',
+          'todos'],
   template: `
     <div>
-      <h2>{{todo.name}}</h2>
-      <button @click="deactivateTodo(todo)">remove</button>
-    </div>`
-});
-
-Vue.component('todo-list', {
-  props: ['todos', 'addTodo', 'deactivateTodo'],
-  template: `<div>
-               <input v-model="newTodo">
-               <button @click="addTodo(newTodo)">Do</button>
-               <button @click="showActive">
-                  Show active
-               </button>
-               <button @click="showInactive">
-                  Show inactive
-               </button>
-               <button @click="showAll">
-                  Show all
-               </button>
-               <ul>
-                 <todo-item 
-                   v-for="todo in filteredTodos"
-                   :deactivate-todo="deactivateTodo"
-                   :todo="todo"
-                   :key="todo.id">
-                 </todo-item>
-               </ul>
-             </div>`,
+      <input v-model="name">
+      <button @click="createTodo(name)">add todo</button>
+      <button @click="showActive">Show Active</button>
+      <button @click="showInactive">Show Inactive</button>
+      <button @click="showAll">Show All</button>
+      <todo-item v-for="todo in filterItems"
+        :todo="todo"
+        :key="todo.id"
+        :deactivate-todo="deactivateTodo"
+      >
+      </todo-item>
+    </div>
+  `,
   computed: {
-    filteredTodos: function(){
-      console.log('showActive: ', this.filterActive);
+    filterItems: function () {
       if(this.filterActive){
         return this.todos.filter((item) => {
-          return item.active 
+          return item.active; 
         });
       }else if(this.filterInactive){
         return this.todos.filter((item) => {
-          return !item.active 
+          return !item.active; 
         });
       }else{
         return this.todos;
       }
     }
   },
-  data: function(){
+  data: function (){
     return {
-      newTodo: '',
-      filterActive: false,
+      name: null,
+      filterActive: true,
       filterInactive: false
-    };
+    }
   },
   methods: {
-    showActive: function(){
-      console.log('show Active!!!');
+    showActive(){
       this.filterActive = true;
       this.filterInactive = false;
     },
-    showInactive: function() {
+    showInactive(){
       this.filterActive = false;
       this.filterInactive = true;
-    },  
-    showAll: function(){
+    },
+    showAll(){
       this.filterActive = false;
       this.filterInactive = false;
     }
   }
 });
 
+var todoItem = Vue.component('todo-item', {
+  props: ['todo', 'deactivateTodo'],
+  template: `
+    <div>
+      {{todo.name}} 
+      <button @click="deactivateTodo(todo.id)">
+        deactivate
+      </button>
+    </div>
+  `
+});
+
 var app = new Vue({
-  el: '#app',
+  el: '#app', 
   data: {
-    todos: data,
-    newTodo: '',
-    price: ''
+    todos: data 
   },
   methods: {
-    addTodo: function (name){
-      console.log('name: ', name);
-      this.todos.push({name: name, 
-        active: true});
+    createTodo(name){
+      this.todos.push({id: this.todos.length + 1, 
+        name: name, active: true});
     },
-    deactivateTodo: function(todo){
-      let result = this.todos.filter((item) => {
-        if(item.id === todo.id){
-          item.active = false; 
-        }
-        return item;
-      });
-      this.todos = result;
+    deactivateTodo(id){
+      this.todos[id - 1].active = false; 
     }
   }
 });
+
+
